fecdb54f89039467dbce57fea161019c1c582a01
JakhongirIsaev <104231457+JakhongirIsaev@users.noreply.github.com>
2024-12-04 01:03:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ from plotly import graph_objs as go
 
 
 print("All imports are working!")
+
+START = "2018-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Prediction Webapp by Jakhongir")
+
+stocks = ("KRW", "AAPL", "GOOG", "MSFT", "GME", "NVDA", "TSLA", "NFLX", "BTC-USD", "META", "AMZN", "DOGE-USD")
+selected_stocks  = st.selectbox("Select dataset fro prediction", stocks)
+
+n_years = st.slider("Years of prediction:", 1 , 4)
+period = n_years * 365
