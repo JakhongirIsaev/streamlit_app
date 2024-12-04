1c4db07e8371406a6e5d00956c1e349f023485f8
JakhongirIsaev <104231457+JakhongirIsaev@users.noreply.github.com>
2024-12-04 13:07:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,34 @@ data = load_data(selected_stocks)
 data_load_state.text("Loading data.. done!")
 
 st.subheader("Raw data")
-st.write(data.tail)
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x = data['Date'], y = data['Open'], name='stock_open' ))
+    fig.add_trace(go.Scatter(x = data['Date'], y = data['Close'], name='stock_close' ))
+    fig.layout.update(title_text = "Time Series Data", xaris_rangeslider_visible = True )
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+#Forcasting 
+
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns = {"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods = period)
+forecast = m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write("forecast data")
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+st.write("forecast componets")
+
+fig2 = m.plot_components(forecast)
+st.write(fig2)
